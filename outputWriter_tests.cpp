3cf172b6bff25d4a215a99f6a2f4e1fd404b2097
LYND$KG <60162813+lyndskg@users.noreply.github.com>
2023-07-20 19:30:11 -0500

    Create outputWriter_tests.cpp

---
+++ outputWriter_tests.cpp
@@ -0,0 +1,17 @@
+//
+//  outputWriter_tests.cpp
+//  black-scholes
+//
+//  Created by lyndskg on 7/19/23.
+
+
+#include <stdio.h>
+#include <iostream>
+#include <cassert>
+
+#include "outputWriter.h"
+#include "unit_test_framework.h"
+
+using namespace std;
+
+using Test_func_t = void (*)();
