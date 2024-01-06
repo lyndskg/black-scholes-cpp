6779621c8bbfc300644d014a8bd57688edf766fe
lyndskg <lyndskg@umich.edu>
2024-01-06 18:53:14 -0500

    AIYA

---
+++ project-defaults.cmake
@@ -0,0 +1,23 @@
+
+set(CMAKE_CXX_STANDARD 20)
+set(CMAKE_CXX_VISIBILITY_PRESET hidden)
+set(CMAKE_POSITION_INDEPENDENT_CODE ON)
+
+add_compile_definitions($<$<CONFIG:DEBUG>:DEBUG>)
+
+add_compile_options(
+        # Don't omit frame pointers (e.g. for crash dumps)
+        -fno-omit-frame-pointer
+        # Enable exception handling
+        -fexceptions
+        # Enable warnings and warnings as errors
+        -Wall
+        -Wextra
+        -Werror
+        -Wconversion
+        # Use -O2 (prioritize speed)
+        $<$<CONFIG:RELEASE>:-O2>
+        # Enable separate sections per function/data item
+        $<$<CONFIG:RELEASE>:-ffunction-sections>
+        $<$<CONFIG:RELEASE>:-fdata-sections>
+)
